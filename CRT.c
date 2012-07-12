6c71b7ed2dc6029e969b3329b6474b30e5e08c5b
Hisham Muhammad <hisham@gobolinux.org>
2012-07-12 16:20:16 +0000

    improve crash message

--- CRT.c
+++ CRT.c
@@ -131,6 +131,11 @@ static void CRT_handleSIGSEGV(int sgn) {
    size_t size = backtrace(backtraceArray, sizeof(backtraceArray) / sizeof(void *));
    fprintf(stderr, "Backtrace: \n");
    backtrace_symbols_fd(backtraceArray, size, 2);
+   fprintf(stderr, "\nAdditionally, in order to make the above backtrace useful,");
+   fprintf(stderr, "\nplease also run the following command to generate a disassembly of your binary:");
+   fprintf(stderr, "\n\n   objdump -d `which htop` > ~/htop.objdump");
+   fprintf(stderr, "\n\nand then attach the file ~/htop.objdump to your bug report.");
+   fprintf(stderr, "\n\nThank you for helping to improve htop!\n\n");
    #endif
    #else
    fprintf(stderr, "\n\nhtop " VERSION " aborting. Unsupported platform.\n");
