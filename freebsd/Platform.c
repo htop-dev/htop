a9f05c2a8bc259a6da0be4004d28c75a9cfe7838
Hisham Muhammad <hisham@gobolinux.org>
2014-11-27 19:33:37 -0200

    Uptime meter for FreeBSD. This will produce too much replicated code. I think I'll use a lighter abstraction in things like this.

--- freebsd/Platform.c
+++ freebsd/Platform.c
@@ -1,11 +1,21 @@
 /*
-htop - unsupported/Platform.c
+htop - freebsd/Platform.c
 (C) 2014 Hisham H. Muhammad
 Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
 */
 
 #include "Platform.h"
+#include "Meter.h"
+#include "CPUMeter.h"
+#include "MemoryMeter.h"
+#include "SwapMeter.h"
+#include "TasksMeter.h"
+#include "LoadAverageMeter.h"
+#include "UptimeMeter.h"
+#include "BatteryMeter.h"
+#include "ClockMeter.h"
+#include "HostnameMeter.h"
 
 /*{
 #include "Action.h"
@@ -14,4 +24,25 @@ in the source distribution for its full text.
 void Platform_setBindings(Htop_Action* keys) {
    (void) keys;
 }
+
+MeterClass* Platform_meterTypes[] = {
+   &CPUMeter_class,
+   &ClockMeter_class,
+   &LoadAverageMeter_class,
+   &LoadMeter_class,
+   &MemoryMeter_class,
+   &SwapMeter_class,
+   &TasksMeter_class,
+   &UptimeMeter_class,
+   &BatteryMeter_class,
+   &HostnameMeter_class,
+   &AllCPUsMeter_class,
+   &AllCPUs2Meter_class,
+   &LeftCPUsMeter_class,
+   &RightCPUsMeter_class,
+   &LeftCPUs2Meter_class,
+   &RightCPUs2Meter_class,
+   &BlankMeter_class,
+   NULL
+};
 
--- freebsd/Platform.h
+++ freebsd/Platform.h
@@ -3,7 +3,7 @@
 #ifndef HEADER_Platform
 #define HEADER_Platform
 /*
-htop - unsupported/Platform.h
+htop - freebsd/Platform.h
 (C) 2014 Hisham H. Muhammad
 Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
@@ -12,6 +12,8 @@ in the source distribution for its full text.
 #include "Action.h"
 
 void Platform_setBindings(Htop_Action* keys);
+
+extern MeterClass* Platform_meterTypes[];
 
 
 #endif
