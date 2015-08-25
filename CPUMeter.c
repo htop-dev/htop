b073520ea880c63cbbf422ff05cda29ac6ac5ccb
Christian Hesse <mail@eworm.de>
2015-08-25 15:38:53 +0200

    align single digit cpu id to center

--- CPUMeter.c
+++ CPUMeter.c
@@ -24,7 +24,8 @@ static void CPUMeter_init(Meter* this) {
    int cpu = this->param;
    if (this->pl->cpuCount > 1) {
       char caption[10];
-      xSnprintf(caption, sizeof(caption), "%-3d", Settings_cpuId(this->pl->settings, cpu - 1));
+      xSnprintf(caption, sizeof(caption), "%2d%s",
+         Settings_cpuId(this->pl->settings, cpu - 1), (cpu - 1 < 100 ? " " : ""));
       Meter_setCaption(this, caption);
    }
    if (this->param == 0)
