0c2ccde3060827528e2ad89159b9d737228df85d
Hisham Muhammad <hisham@gobolinux.org>
2015-09-11 13:39:06 -0300

    Detect when libtoolize is called glibtoolize. It's the case on Darwin, and might be in other BSDs as well. As referred in #268. Supersedes PR #269.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+if glibtoolize --version &> /dev/null
+then libtoolize=glibtoolize
+else libtoolize=libtoolize
+fi
+
 aclocal -I m4
 autoconf
 autoheader
-libtoolize --copy --force
+$libtoolize --copy --force
 automake --add-missing --copy
-
 
