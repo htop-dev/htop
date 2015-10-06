3ea7b3473583c15747d36323c1b1db4ee5cdc4e6
Martin "eto" Misuth <et.code@ethome.sk>
2015-10-06 19:39:01 +0200

    attempt to return to upstream/master

--- SignalsPanel.c
+++ SignalsPanel.c
@@ -14,6 +14,7 @@ in the source distribution for its full text.
 
 #include <stdlib.h>
 #include <assert.h>
+#include <signal.h>
 
 #include <ctype.h>
 
@@ -28,9 +29,16 @@ typedef struct SignalItem_ {
 
 Panel* SignalsPanel_new() {
    Panel* this = Panel_new(1, 1, 1, 1, true, Class(ListItem), FunctionBar_newEnterEsc("Send   ", "Cancel "));
-   for(unsigned int i = 0; i < Platform_numberOfSignals; i++)
+   const int defaultSignal = SIGTERM;
+   int defaultPosition = 15;
+   for(unsigned int i = 0; i < Platform_numberOfSignals; i++) {
       Panel_set(this, i, (Object*) ListItem_new(Platform_signals[i].name, Platform_signals[i].number));
+      // signal 15 is not always the 15th signal in the table
+      if (Platform_signals[i].number == defaultSignal) {
+         defaultPosition = i;
+      }
+   }
    Panel_setHeader(this, "Send signal:");
-   Panel_setSelected(this, DEFAULT_SIGNAL);
+   Panel_setSelected(this, defaultPosition);
    return this;
 }
