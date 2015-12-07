ab3a7c2fa826932c9c297885b0ea33f1d880cc01
Michael Klein <michael.klein@puffin.lb.shuttle.de>
2015-12-07 20:10:09 +0100

    drop privileges before changing process priority or sending signals
    - replaces uid check from d18e9a4895599a479df264a6c7380b8805abb434

--- Process.c
+++ Process.c
@@ -513,16 +513,15 @@ void Process_toggleTag(Process* this) {
 }
 
 bool Process_setPriority(Process* this, int priority) {
-   if ( Process_getuid == 0 || Process_getuid == (int) this->st_uid ) {
-      int old_prio = getpriority(PRIO_PROCESS, this->pid);
-      int err = setpriority(PRIO_PROCESS, this->pid, priority);
-      if (err == 0 && old_prio != getpriority(PRIO_PROCESS, this->pid)) {
-         this->nice = priority;
-      }
-      return (err == 0);
+   uid_t euid = geteuid();
+   seteuid(getuid());
+   int old_prio = getpriority(PRIO_PROCESS, this->pid);
+   int err = setpriority(PRIO_PROCESS, this->pid, priority);
+   seteuid(euid);
+   if (err == 0 && old_prio != getpriority(PRIO_PROCESS, this->pid)) {
+      this->nice = priority;
    }
-   else
-      return false;
+   return (err == 0);
 }
 
 bool Process_changePriorityBy(Process* this, size_t delta) {
@@ -530,8 +529,10 @@ bool Process_changePriorityBy(Process* this, size_t delta) {
 }
 
 void Process_sendSignal(Process* this, size_t sgn) {
-   if ( Process_getuid == 0 || Process_getuid == (int) this->st_uid )
-      kill(this->pid, (int) sgn);
+   uid_t euid = geteuid();
+   seteuid(getuid());
+   kill(this->pid, (int) sgn);
+   seteuid(euid);
 }
 
 long Process_pidCompare(const void* v1, const void* v2) {
