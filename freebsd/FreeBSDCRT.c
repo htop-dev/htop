71703827063770d78438ec0051e70f579faafbf4
Michael McConville <mmcco@mykolab.com>
2016-01-02 12:11:26 -0500

    Fix spelling of "maintainer"

--- freebsd/FreeBSDCRT.c
+++ freebsd/FreeBSDCRT.c
@@ -15,7 +15,7 @@ void CRT_handleSIGSEGV(int sgn) {
    CRT_done();
    fprintf(stderr, "\n\nhtop " VERSION " aborting.\n");
    fprintf(stderr, "\nUnfortunately, you seem to be using an unsupported platform!");
-   fprintf(stderr, "\nPlease contact your platform package mantainer!\n\n");
+   fprintf(stderr, "\nPlease contact your platform package maintainer!\n\n");
    abort();
 }
 
