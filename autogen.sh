471cd6063502989be8f6f0db4eb3ae722cd6eddf
Max Horn <max@quendi.de>
2016-02-14 22:09:52 +0100

    Simplify autogen.sh
    Make use of the autoreconf tool

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-
-glibtoolize=$(which glibtoolize 2> /dev/null)
-if [ ${#glibtoolize} -gt 0 ]
-then libtoolize=glibtoolize
-else libtoolize=libtoolize
-fi
-
 mkdir -p m4
-aclocal -I m4
-autoconf
-autoheader
-$libtoolize --copy --force
-automake --add-missing --copy
-
+autoreconf --install --force
