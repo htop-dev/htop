53d7c66ac3081a2db7bca72876e3e16b89416ea2
coypoop <coypu@sdf.org>
2017-03-25 14:12:26 +0300

    Add missing include
    sys/time.h for struct timeval

--- TraceScreen.c
+++ TraceScreen.c
@@ -22,6 +22,7 @@ in the source distribution for its full text.
 #include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
