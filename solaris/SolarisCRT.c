4bd68809e4eda7b95b509e14a1e59a8b9618bb63
Guy M. Broome <thrirhrafnir@gmail.com>
2018-03-05 11:39:37 -0500

    Solaris: Import backtrace-on-abort from Linux, with minor modification for Solaris

--- solaris/SolarisCRT.c
+++ solaris/SolarisCRT.c
@@ -1,6 +1,7 @@
 /*
 htop - SolarisCRT.c
 (C) 2014 Hisham H. Muhammad
+(C) 2018 Guy M. Broome
 Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
 */
@@ -9,13 +10,23 @@ in the source distribution for its full text.
 #include "CRT.h"
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef HAVE_EXECINFO_H
+#include <execinfo.h>
+#endif
 
 void CRT_handleSIGSEGV(int sgn) {
    (void) sgn;
    CRT_done();
-   fprintf(stderr, "\n\nhtop " VERSION " aborting.\n");
-   fprintf(stderr, "\nUnfortunately, you seem to be using an unsupported platform!");
-   fprintf(stderr, "\nPlease contact your platform package maintainer!\n\n");
+   fprintf(stderr, "\n\nhtop " VERSION " aborting. Please report bug at http://hisham.hm/htop\n");
+   #ifdef HAVE_EXECINFO_H
+   size_t size = backtrace(backtraceArray, sizeof(backtraceArray) / sizeof(void *));
+   fprintf(stderr, "\n Please include in your report the following backtrace: \n");
+   backtrace_symbols_fd(backtraceArray, size, 2);
+   fprintf(stderr, "\nAdditionally, in order to make the above backtrace useful,");
+   fprintf(stderr, "\nplease also run the following command to generate a disassembly of your binary:");
+   fprintf(stderr, "\n\n   objdump -d `which htop` > ~/htop.objdump");
+   fprintf(stderr, "\n\nand then attach the file ~/htop.objdump to your bug report.");
+   fprintf(stderr, "\n\nThank you for helping to improve htop!\n\n");
+   #endif
    abort();
 }
-
--- solaris/SolarisCRT.h
+++ solaris/SolarisCRT.h
@@ -5,11 +5,14 @@
 /*
 htop - SolarisCRT.h
 (C) 2014 Hisham H. Muhammad
+(C) 2018 Guy M. Broome
 Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
 */
 
-void CRT_handleSIGSEGV(int sgn);
+#ifdef HAVE_EXECINFO_H
+#endif
 
+void CRT_handleSIGSEGV(int sgn);
 
 #endif
