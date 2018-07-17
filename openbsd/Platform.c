48b807b0ff892d41387f6cedfb3668e2bcc246ac
Ian Sutton <ians@openbsd.org>
2018-07-17 08:43:50 -0500

    Fix CPU meters
    Introduction of CP_SPIN sched state broke hard-coded state indexes
    resulting in the meters incorrectly reporting bogus intr data instead of
    CPU usage. Change hardcoded values to sched.h macros.

--- openbsd/Platform.c
+++ openbsd/Platform.c
@@ -46,7 +46,7 @@ extern ProcessFieldData Process_fields[];
 
 #define MAXCPU 256
 // XXX: probably should be a struct member
-static int64_t old_v[MAXCPU][5];
+static int64_t old_v[MAXCPU][CPUSTATES];
 
 /*
  * Copyright (c) 1984, 1989, William LeFebvre, Rice University
@@ -225,7 +225,7 @@ double Platform_setCPUValues(Meter* this, int cpu) {
       v[i] = diff_v[i] / 10.;
    }
 
-   Meter_setItems(this, 4);
+   Meter_setItems(this, CP_IDLE);
 
    perc = v[0] + v[1] + v[2] + v[3];
 
