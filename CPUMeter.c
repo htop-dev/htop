0f5d2cd1e456b251706ebd337cacc8593186819f
Redha Gouicem <redha.gouicem@gmail.com>
2020-08-31 13:32:29 +0200

    fixed compilation error

--- CPUMeter.c
+++ CPUMeter.c
@@ -70,7 +70,7 @@ static void CPUMeter_updateValues(Meter* this, char* buffer, int size) {
       if (cpuFrequency < 0) {
          xSnprintf(cpuFrequencyBuffer, sizeof(cpuFrequencyBuffer), "N/A");
       } else {
-         xSnprintf(cpuFrequencyBuffer, sizeof(cpuFrequencyBuffer), "%.0f%cHz", cpuFrequency, unit);
+         xSnprintf(cpuFrequencyBuffer, sizeof(cpuFrequencyBuffer), "%.0fGHz", cpuFrequency);
       }
       if (this->pl->settings->showCPUUsage) {
          xSnprintf(buffer, size, "%5.1f%% %s", percent, cpuFrequencyBuffer);
