ce0fd5f6d8769e5d482fc553bdbc22fbdb518501
Christian Göttsche <cgzones@googlemail.com>
2020-09-23 12:39:45 +0200

    Drop unused macros

--- XAlloc.c
+++ XAlloc.c
@@ -39,20 +39,6 @@ void* xRealloc(void* ptr, size_t size) {
    return data;
 }
 
-#undef xAsprintf
-
-#define xAsprintf(strp, fmt, ...) do { int _r=asprintf(strp, fmt, __VA_ARGS__); if (_r < 0) { fail(); } } while(0)
-
-#define xSnprintf(fmt, len, ...) do { int _l=len; int _n=snprintf(fmt, _l, __VA_ARGS__); if (!(_n > -1 && _n < _l)) { curs_set(1); endwin(); err(1, NULL); } } while(0)
-
-#undef xStrdup
-#undef xStrdup_
-#ifdef NDEBUG
-# define xStrdup_ xStrdup
-#else
-# define xStrdup(str_) (assert(str_), xStrdup_(str_))
-#endif
-
 char* xStrdup_(const char* str) {
    char* data = strdup(str);
    if (!data) {
