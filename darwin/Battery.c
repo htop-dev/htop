059810ca65385d3feb3fc67857437bd740641c53
Christian Göttsche <cgzones@googlemail.com>
2020-10-27 11:50:18 +0100

    Drop always true condition

--- darwin/Battery.c
+++ darwin/Battery.c
@@ -17,59 +17,57 @@ void Battery_getData(double* level, ACPresence* isOnAC) {
       return;
    }
 
-   if(power_sources != NULL) {
-      CFArrayRef list = IOPSCopyPowerSourcesList(power_sources);
-      CFDictionaryRef battery = NULL;
-      int len;
+   CFArrayRef list = IOPSCopyPowerSourcesList(power_sources);
+   CFDictionaryRef battery = NULL;
+   int len;
 
-      if(NULL == list) {
-         CFRelease(power_sources);
+   if(NULL == list) {
+      CFRelease(power_sources);
 
-         return;
-      }
+      return;
+   }
 
-      len = CFArrayGetCount(list);
+   len = CFArrayGetCount(list);
 
-      /* Get the battery */
-      for(int i = 0; i < len && battery == NULL; ++i) {
-         CFDictionaryRef candidate = IOPSGetPowerSourceDescription(power_sources,
-                                     CFArrayGetValueAtIndex(list, i)); /* GET rule */
-         CFStringRef type;
+   /* Get the battery */
+   for(int i = 0; i < len && battery == NULL; ++i) {
+      CFDictionaryRef candidate = IOPSGetPowerSourceDescription(power_sources,
+                                    CFArrayGetValueAtIndex(list, i)); /* GET rule */
+      CFStringRef type;
 
-         if(NULL != candidate) {
-            type = (CFStringRef) CFDictionaryGetValue(candidate,
-                   CFSTR(kIOPSTransportTypeKey)); /* GET rule */
+      if(NULL != candidate) {
+         type = (CFStringRef) CFDictionaryGetValue(candidate,
+                  CFSTR(kIOPSTransportTypeKey)); /* GET rule */
 
-            if(kCFCompareEqualTo == CFStringCompare(type, CFSTR(kIOPSInternalType), 0)) {
-               CFRetain(candidate);
-               battery = candidate;
-            }
+         if(kCFCompareEqualTo == CFStringCompare(type, CFSTR(kIOPSInternalType), 0)) {
+            CFRetain(candidate);
+            battery = candidate;
          }
       }
-
-      if(NULL != battery) {
-         /* Determine the AC state */
-         CFStringRef power_state = CFDictionaryGetValue(battery, CFSTR(kIOPSPowerSourceStateKey));
-
-         *isOnAC = (kCFCompareEqualTo == CFStringCompare(power_state, CFSTR(kIOPSACPowerValue), 0))
-                 ? AC_PRESENT
-                 : AC_ABSENT;
-
-         /* Get the percentage remaining */
-         double current;
-         double max;
-
-         CFNumberGetValue(CFDictionaryGetValue(battery, CFSTR(kIOPSCurrentCapacityKey)),
-                 kCFNumberDoubleType, &current);
-         CFNumberGetValue(CFDictionaryGetValue(battery, CFSTR(kIOPSMaxCapacityKey)),
-                 kCFNumberDoubleType, &max);
-
-         *level = (current * 100.0) / max;
-
-         CFRelease(battery);
-      }
-
-      CFRelease(list);
-      CFRelease(power_sources);
    }
+
+   if(NULL != battery) {
+      /* Determine the AC state */
+      CFStringRef power_state = CFDictionaryGetValue(battery, CFSTR(kIOPSPowerSourceStateKey));
+
+      *isOnAC = (kCFCompareEqualTo == CFStringCompare(power_state, CFSTR(kIOPSACPowerValue), 0))
+               ? AC_PRESENT
+               : AC_ABSENT;
+
+      /* Get the percentage remaining */
+      double current;
+      double max;
+
+      CFNumberGetValue(CFDictionaryGetValue(battery, CFSTR(kIOPSCurrentCapacityKey)),
+               kCFNumberDoubleType, &current);
+      CFNumberGetValue(CFDictionaryGetValue(battery, CFSTR(kIOPSMaxCapacityKey)),
+               kCFNumberDoubleType, &max);
+
+      *level = (current * 100.0) / max;
+
+      CFRelease(battery);
+   }
+
+   CFRelease(list);
+   CFRelease(power_sources);
 }
