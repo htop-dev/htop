4283679e6085e2304c9a0af06fa16a83dea69762
Christian Göttsche <cgzones@googlemail.com>
2020-11-04 17:46:14 +0100

    Mark local functions static

--- linux/LinuxProcess.h
+++ linux/LinuxProcess.h
@@ -181,10 +181,6 @@ bool LinuxProcess_setIOPriority(Process* this, Arg ioprio);
 void LinuxProcess_printDelay(float delay_percent, char* buffer, int n);
 #endif
 
-void LinuxProcess_writeField(const Process* this, RichString* str, ProcessField field);
-
-long LinuxProcess_compare(const void* v1, const void* v2);
-
 bool Process_isThread(const Process* this);
 
 #endif
