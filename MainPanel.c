edd6130be73d1cb847f65b06409ac6e0b9cd9d61
Christian Göttsche <cgzones@googlemail.com>
2020-12-15 16:59:07 +0100

    MainPanel: use actual KEY_RESIZE instead of KEY_SHUFFLE
    KEY_RESIZE (0632) is equal to KEY_SHUFFLE (0x19a)

--- MainPanel.c
+++ MainPanel.c
@@ -51,7 +51,7 @@ static HandlerResult MainPanel_eventHandler(Panel* super, int ch) {
 
    Htop_Reaction reaction = HTOP_OK;
 
-   if (ch != ERR && ch != /* KEY_SHUFFLE */ 0x19a)
+   if (ch != ERR && ch != KEY_RESIZE)
       this->state->hideProcessSelection = false;
 
    if (EVENT_IS_HEADER_CLICK(ch)) {
