36389fb0dac5c1aed0fe0f6ccbd676ad364c1a9b
Nathan Scott <nathans@redhat.com>
2021-03-23 15:04:54 +1100

    Abstract htop main function to allow for a platform binary
    One review request relating to the PCP platform is to have
    a clearly separate binary from the regular htop so that we
    have no confusion as to what is being requested to run, to
    aid debugging, and a bunch of other good reasons.
    
    This commit renames htop.c to CommandLine.c and provides a
    minimal htop main function for 'native' platforms to use.
    The PCP version of this will setup libpcp.so and then call
    the same CommandLine_run function as regular htop.
    
    Related to https://github.com/htop-dev/htop/pull/536

--- CommandLine.h
+++ CommandLine.h
@@ -1,16 +1,14 @@
+#ifndef HEADER_CommandLine
+#define HEADER_CommandLine
 /*
-htop - htop.c
+htop - CommandLine.h
 (C) 2004-2011 Hisham H. Muhammad
 (C) 2020-2021 htop dev team
 Released under the GNU GPLv2, see the COPYING file
 in the source distribution for its full text.
 */
 
-#include "config.h" // IWYU pragma: keep
 
-#include "CommandLine.h"
+int CommandLine_run(const char* name, int argc, char** argv);
 
-
-int main(int argc, char** argv) {
-   return CommandLine_run(PACKAGE, argc, argv);
-}
+#endif
