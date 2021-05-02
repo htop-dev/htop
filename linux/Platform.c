8120b82a57d7c59f9e29beb83a5aa0582e19d7a8
Christian Göttsche <cgzones@googlemail.com>
2021-05-02 17:41:15 +0200

    Add meter to display any libsensors fan speed
    Show the input, minimum and maximum fan speed of any available
    libsensors fan sensor.

--- linux/Platform.c
+++ linux/Platform.c
@@ -73,6 +73,7 @@ in the source distribution for its full text.
 
 #ifdef HAVE_SENSORS_SENSORS_H
 #include "linux/LibSensors.h"
+#include "linux/LibSensorsFanMeter.h"
 #include "linux/LibSensorsTempMeter.h"
 #endif
 
@@ -204,6 +205,7 @@ const MeterClass* const Platform_meterTypes[] = {
    &NetworkIOMeter_class,
    &NetworkInterfaceIOMeter_class,
 #ifdef HAVE_SENSORS_SENSORS_H
+   &LibSensorsFanMeter_class,
    &LibSensorsTempMeter_class,
 #endif
    &SELinuxMeter_class,
