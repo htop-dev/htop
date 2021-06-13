adcedf87f5ac09e640179d8df3a4da1d9cb9f9a9
Christian Göttsche <cgzones@googlemail.com>
2021-06-13 11:20:54 +0200

    FreeBSD: mark the original kernel thread with pid 0 as such

--- freebsd/FreeBSDProcessList.c
+++ freebsd/FreeBSDProcessList.c
@@ -498,7 +498,7 @@ void ProcessList_goThroughEntries(ProcessList* super, bool pauseProcessUpdate) {
       if (!preExisting) {
          fp->jid = kproc->ki_jid;
          proc->pid = kproc->ki_pid;
-         proc->isKernelThread = kproc->ki_pid != 0 && kproc->ki_pid != 1 && (kproc->ki_flag & P_SYSTEM);
+         proc->isKernelThread = kproc->ki_pid != 1 && (kproc->ki_flag & P_SYSTEM);
          proc->isUserlandThread = false;
          proc->ppid = kproc->ki_ppid;
          proc->tpgid = kproc->ki_tpgid;
