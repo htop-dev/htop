fcca4c2f2df8f6d696936a89e5c24e9b4d8a7312
Christian Göttsche <cgzones@googlemail.com>
2021-09-10 11:26:45 +0200

    valgrind: track origin of uninitialised values

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -3,4 +3,4 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "$SCRIPT")
 
-valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-fds=yes --errors-for-leak-kinds=all --suppressions="${SCRIPTDIR}/htop_suppressions.valgrind" "${SCRIPTDIR}/../htop"
+valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-fds=yes --errors-for-leak-kinds=all --track-origins=yes --suppressions="${SCRIPTDIR}/htop_suppressions.valgrind" "${SCRIPTDIR}/../htop"
