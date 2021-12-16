5b78ad2d53b751e8b07d67ab8070e47f77036b84
Christian Göttsche <cgzones@googlemail.com>
2021-12-16 16:09:32 +0100

    Set correct default sorting direction
    Respect the field option defaultSortDesc for the default screen sort
    direction, e.g. for CPU%.

--- ScreensPanel.c
+++ ScreensPanel.c
@@ -149,7 +149,7 @@ static void addNewScreen(Panel* super) {
    ScreensPanel* const this = (ScreensPanel*) super;
 
    const char* name = "New";
-   ScreenSettings* ss = Settings_newScreen(this->settings, name, "PID Command");
+   ScreenSettings* ss = Settings_newScreen(this->settings, &(const ScreenDefaults){ .name = name, .columns = "PID Command", .sortKey = "PID" });
    ScreenListItem* item = ScreenListItem_new(name, ss);
    int idx = Panel_getSelectedIndex(super);
    Panel_insert(super, idx + 1, (Object*) item);
