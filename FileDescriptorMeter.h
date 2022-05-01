e0229b205c5dceb10447457c8f78acb16c96505a
Benny Baumann <BenBE@geshi.org>
2022-05-01 22:43:00 +0200

    Create new File Descriptor meter

---
+++ FileDescriptorMeter.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FileDescriptorMeter
+#define HEADER_FileDescriptorMeter
+/*
+htop - FileDescriptorMeter.h
+(C) 2022 htop dev team
+Released under the GNU GPLv2+, see the COPYING file
+in the source distribution for its full text.
+*/
+
+#include "Meter.h"
+
+
+extern const MeterClass FileDescriptorMeter_class;
+
+#endif
