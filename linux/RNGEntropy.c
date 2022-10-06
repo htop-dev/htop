1e5509256a23c86fb4ba1db6b11d8306444fe0b2
Loïc Jourdheuil Sellin <lojs@ic.fr>
2022-10-06 12:43:07 +0200

    Use xReadfile and strtoul to read value.
    Add more test to set poolsize or entropy to INVALIDE_VALUE if there are
    an error when read files.
    In RNGEntropy_display, print N/A if poolsize or entropy have invalid value
    Add newline @ EOF.

--- linux/RNGEntropy.c
+++ linux/RNGEntropy.c
@@ -7,65 +7,67 @@ in the source distribution for its full text.
 
 #include "linux/RNGEntropy.h"
 
+#include <stdlib.h>
+
 #include "CRT.h"
-
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/statfs.h>
-#include <sys/statvfs.h>
-
 #include "Object.h"
 #include "XUtils.h"
 
-static unsigned int entropy = 0;
-static unsigned int poolsize = 0;
+#define INVALID_VALUE ((unsigned int)-1)
 
-static void getRNGEntropy(void) {
-   FILE* file = fopen(PROCDIR "/sys/kernel/random/entropy_avail", "r");
-   if (!file)
-      return;
-   if (!fscanf(file, "%u", &entropy))
-      entropy = 0;
-   fclose(file);
-}
-
-static void getRNGPoolsize(void) {
-   /* Read poolsize only the first time */
-   if (poolsize > 0)
-      return;
-
-   FILE* file = fopen(PROCDIR "/sys/kernel/random/poolsize", "r");
-   if (!file)
-      return;
-   if (!fscanf(file, "%u", &poolsize))
-      poolsize = 0;
-   fclose(file);
-}
+static unsigned int entropy = INVALID_VALUE;
+static unsigned int poolsize = INVALID_VALUE;
 
 static void RNGEntropy_updateValues(Meter* this) {
-   getRNGPoolsize();
-   getRNGEntropy();
+   char buffer[11];
+   ssize_t r;
 
-   if ((poolsize == 0) || (entropy == 0)) {
-      xSnprintf(this->txtBuffer, sizeof(this->txtBuffer), "N/A");
+   if (poolsize == INVALID_VALUE) {
+      r = xReadfile(PROCDIR "/sys/kernel/random/poolsize", buffer, sizeof(buffer));
+      if (r <= 0) {
+         goto err;
+      }
+      poolsize = (unsigned int)strtoul(buffer, NULL, 10);
+      if (poolsize == 0) {
+         /* invalid poolsize value */
+         poolsize = INVALID_VALUE;
+         goto err;
+      }
+   }
+   r = xReadfile(PROCDIR "/sys/kernel/random/entropy_avail", buffer, sizeof(buffer));
+   if (r <= 0) {
+      entropy = INVALID_VALUE;
+      goto err;
+   } else {
+      entropy = (unsigned int)strtoul(buffer, NULL, 10);
    }
 
    this->values[0] = (100.0 * entropy) / poolsize;
    xSnprintf(this->txtBuffer, sizeof(this->txtBuffer), "%.1f%%", this->values[0]);
+   return;
+
+err:
+   xSnprintf(this->txtBuffer, sizeof(this->txtBuffer), "N/A");
+   this->values[0] = 0.0;
 }
 
 static void RNGEntropy_display(ATTR_UNUSED const Object* cast, RichString* out) {
    char buffer[50];
    int len;
 
-   RichString_appendAscii(out, CRT_colors[METER_TEXT], " Entropy: ");
-   len = xSnprintf(buffer, sizeof(buffer), "%u", entropy);
-   RichString_appendnAscii(out, CRT_colors[METER_VALUE_OK], buffer, len);
-   RichString_appendAscii(out, CRT_colors[METER_TEXT], " Poolsize: ");
-   len = xSnprintf(buffer, sizeof(buffer), "%u", poolsize);
-   RichString_appendAscii(out, CRT_colors[METER_VALUE], buffer);
+   if ((poolsize == INVALID_VALUE) || (entropy == INVALID_VALUE)) {
+      RichString_appendAscii(out, CRT_colors[METER_TEXT], " Entropy: ");
+      RichString_appendAscii(out, CRT_colors[METER_VALUE_OK], "N/A");
+      RichString_appendAscii(out, CRT_colors[METER_TEXT], " Poolsize: ");
+      RichString_appendAscii(out, CRT_colors[METER_VALUE], "N/A");
+   } else {
+      RichString_appendAscii(out, CRT_colors[METER_TEXT], " Entropy: ");
+      len = xSnprintf(buffer, sizeof(buffer), "%u", entropy);
+      RichString_appendnAscii(out, CRT_colors[METER_VALUE_OK], buffer, len);
+      RichString_appendAscii(out, CRT_colors[METER_TEXT], " Poolsize: ");
+      len = xSnprintf(buffer, sizeof(buffer), "%u", poolsize);
+      RichString_appendAscii(out, CRT_colors[METER_VALUE], buffer);
+   }
 }
 
 static const int RNGEntropy_attributes[] = {
