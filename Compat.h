1b640dff5759d73458204c87e217dab8759badd9
Ivan Shapovalov <intelfx@intelfx.name>
2023-03-12 20:08:40 +0400

    Compat: implement static_assert() shim for pre-C11

--- Compat.h
+++ Compat.h
@@ -9,6 +9,7 @@ in the source distribution for its full text.
 
 #include "config.h" // IWYU pragma: keep
 
+#include <assert.h> // IWYU pragma: keep
 #include <fcntl.h>
 #include <stddef.h>
 #include <unistd.h>
@@ -60,5 +61,27 @@ ssize_t Compat_readlink(openat_arg_t dirfd,
                         const char* pathname,
                         char* buf,
                         size_t bufsize);
+
+/*
+ * static_assert() hack for pre-C11
+ * TODO: drop after moving to -std=c11 or newer
+ */
+
+/* C11 guarantees _Static_assert is a keyword */
+#if (defined __STDC_VERSION__ ? __STDC_VERSION__ : 0) < 201112L
+# if !defined(_Static_assert)
+#  define _Static_assert(expr, msg)                                    \
+   extern int (*__Static_assert_function (void))                       \
+      [!!sizeof (struct { int __error_if_negative: (expr) ? 2 : -1; })]
+# endif
+#endif
+
+/* C23 will guarantee static_assert is a keyword or a macro */
+/* FIXME: replace 202300L with proper value once C23 is published */
+#if (defined __STDC_VERSION__ ? __STDC_VERSION__ : 0) < 202300L
+# if !defined(static_assert)
+#  define static_assert(expr, msg) _Static_assert(expr, msg)
+# endif
+#endif
 
 #endif /* HEADER_Compat */
