c0055e6083d88de0b78904bd7c29f5c6a2f50d87
Christian Göttsche <cgzones@googlemail.com>
2023-04-03 20:30:20 +0200

    ZramMeter: update bar mode
    Show both compressed and uncompressed bars.
    
    Closes: #1216

--- linux/ZramMeter.c
+++ linux/ZramMeter.c
@@ -7,10 +7,12 @@
 #include "Object.h"
 #include "Platform.h"
 #include "RichString.h"
+#include "ZramMeter.h"
 
 
-static const int ZramMeter_attributes[] = {
-   ZRAM
+static const int ZramMeter_attributes[ZRAM_METER_ITEMCOUNT] = {
+   [ZRAM_METER_COMPRESSED] = ZRAM_COMPRESSED,
+   [ZRAM_METER_UNCOMPRESSED] = ZRAM_UNCOMPRESSED,
 };
 
 static void ZramMeter_updateValues(Meter* this) {
@@ -20,15 +22,12 @@ static void ZramMeter_updateValues(Meter* this) {
 
    Platform_setZramValues(this);
 
-   /* on print bar for compressed data size, not uncompressed */
-   this->curItems = 1;
-
-   written = Meter_humanUnit(buffer, this->values[0], size);
+   written = Meter_humanUnit(buffer, this->values[ZRAM_METER_COMPRESSED], size);
    METER_BUFFER_CHECK(buffer, size, written);
 
    METER_BUFFER_APPEND_CHR(buffer, size, '(');
 
-   written = Meter_humanUnit(buffer, this->values[1], size);
+   written = Meter_humanUnit(buffer, this->values[ZRAM_METER_UNCOMPRESSED], size);
    METER_BUFFER_CHECK(buffer, size, written);
 
    METER_BUFFER_APPEND_CHR(buffer, size, ')');
@@ -47,11 +46,11 @@ static void ZramMeter_display(const Object* cast, RichString* out) {
    Meter_humanUnit(buffer, this->total, sizeof(buffer));
    RichString_appendAscii(out, CRT_colors[METER_VALUE], buffer);
 
-   Meter_humanUnit(buffer, this->values[0], sizeof(buffer));
+   Meter_humanUnit(buffer, this->values[ZRAM_METER_COMPRESSED], sizeof(buffer));
    RichString_appendAscii(out, CRT_colors[METER_TEXT], " used:");
    RichString_appendAscii(out, CRT_colors[METER_VALUE], buffer);
 
-   Meter_humanUnit(buffer, this->values[1], sizeof(buffer));
+   Meter_humanUnit(buffer, this->values[ZRAM_METER_UNCOMPRESSED], sizeof(buffer));
    RichString_appendAscii(out, CRT_colors[METER_TEXT], " uncompressed:");
    RichString_appendAscii(out, CRT_colors[METER_VALUE], buffer);
 }
@@ -64,7 +63,8 @@ const MeterClass ZramMeter_class = {
    },
    .updateValues = ZramMeter_updateValues,
    .defaultMode = BAR_METERMODE,
-   .maxItems = 2,
+   .maxItems = ZRAM_METER_ITEMCOUNT,
+   .comprisedValues = true,
    .total = 100.0,
    .attributes = ZramMeter_attributes,
    .name = "Zram",
