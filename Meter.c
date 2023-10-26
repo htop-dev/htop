htop-dev/htop

commit 74b28849c4ee7234aa6018667ff6c8c9a6b41273

Let GraphMeterMode_draw() return early when the meter is too narrow...
...to draw even a single record of the graph.

This makes the code more robust by checking (w <= 0) explicitly.
Also avoids unnecessary reallocations of the GraphData buffer.

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

Meter.c
@@ -289,10 +289,18 @@ static const char* const GraphMeterMode_dotsAscii[] = {
 };
 
 static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
+   const char* caption = Meter_getCaption(this);
+   attrset(CRT_colors[METER_TEXT]);
+   const int captionLen = 3;
+   mvaddnstr(y, x, caption, captionLen);
+   x += captionLen;
+   w -= captionLen;
+   if (w <= 0)
+      return;
+
    const Machine* host = this->host;
    GraphData* data = &this->drawData;
 
-   assert(w > 0);
    if ((size_t)w * 2 > data->nValues) {
       size_t oldNValues = data->nValues;
       data->nValues = MAXIMUM(oldNValues + (oldNValues / 2), (size_t)w * 2);
@@ -314,13 +322,6 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
       GraphMeterMode_dots = GraphMeterMode_dotsAscii;
       GraphMeterMode_pixPerRow = PIXPERROW_ASCII;
    }
-
-   const char* caption = Meter_getCaption(this);
-   attrset(CRT_colors[METER_TEXT]);
-   int captionLen = 3;
-   mvaddnstr(y, x, caption, captionLen);
-   x += captionLen;
-   w -= captionLen;
 
    if (!timercmp(&host->realtime, &(data->time), <)) {
       int globalDelay = this->host->settings->delay;

commit 9bf272c8586cbd244d2a2f0817fe9d6fa2eaa01e

Record new graph data even when no graph is drawn.
Fixes a regression caused by 74b28849c4ee7234aa6018667ff6c8c9a6b41273.

When a graph meter is too narrow to draw the graph, but the graph data
buffer is allocated, the meter should keep recording new values.

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

Meter.c
@@ -10,6 +10,7 @@ in the source distribution for its full text.
 #include "Meter.h"
 
 #include <assert.h>
+#include <limits.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -295,11 +296,10 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
    mvaddnstr(y, x, caption, captionLen);
    x += captionLen;
    w -= captionLen;
-   if (w <= 0)
-      return;
 
    GraphData* data = &this->drawData;
-   if ((size_t)w * 2 > data->nValues && MAX_METER_GRAPHDATA_VALUES > data->nValues) {
+   assert(data->nValues / 2 <= INT_MAX);
+   if (w > (int)(data->nValues / 2) && MAX_METER_GRAPHDATA_VALUES > data->nValues) {
       size_t oldNValues = data->nValues;
       data->nValues = MAXIMUM(oldNValues + oldNValues / 2, (size_t)w * 2);
       data->nValues = MINIMUM(data->nValues, MAX_METER_GRAPHDATA_VALUES);
@@ -307,11 +307,10 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
       memmove(data->values + (data->nValues - oldNValues), data->values, oldNValues * sizeof(*data->values));
       memset(data->values, 0, (data->nValues - oldNValues) * sizeof(*data->values));
    }
+
    const size_t nValues = data->nValues;
-   if ((size_t)w * 2 > nValues) {
-      x += w - nValues / 2;
-      w = nValues / 2;
-   }
+   if (nValues < 1)
+      return;
 
    const Machine* host = this->host;
    if (!timercmp(&host->realtime, &(data->time), <)) {
@@ -323,6 +322,14 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
          data->values[i] = data->values[i + 1];
 
       data->values[nValues - 1] = sumPositiveValues(this->values, this->curItems);
+   }
+
+   if (w <= 0)
+      return;
+
+   if ((size_t)w > nValues / 2) {
+      x += w - nValues / 2;
+      w = nValues / 2;
    }
 
    const char* const* GraphMeterMode_dots;