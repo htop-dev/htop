179aeb01082df9e7c19a285ae4d703a7714f4e2f
Daniel Lange <DLange@git.local>
2023-11-29 17:44:20 +0100

    Add includes for config.h as per the discussion in PR #1337
    Many thanks to @Explorer09 Kang-Che Sung (宋岡哲).
    
    Also add a #error stanza to XUtils.h in case somebody forgets the beautiful mess GNU forces on us.

--- OptionItem.c
+++ OptionItem.c
@@ -5,6 +5,8 @@ Released under the GNU GPLv2+, see the COPYING file
 in the source distribution for its full text.
 */
 
+#include "config.h" // IWYU pragma: keep
+
 #include "OptionItem.h"
 
 #include <assert.h>
--- netbsd/Platform.c
+++ netbsd/Platform.c
@@ -9,6 +9,8 @@ Released under the GNU GPLv2+, see the COPYING file
 in the source distribution for its full text.
 */
 
+#include "config.h" // IWYU pragma: keep
+
 #include "netbsd/Platform.h"
 
 #include <errno.h>
