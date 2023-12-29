htop-dev/htop

commit c55401b82b1bb278947f9d2a3aed13a9f0de85a9

Add support for "truss" (FreeBSD equivalent of "strace")

TraceScreen.c
@@ -90,13 +90,24 @@ bool TraceScreen_forkTracer(TraceScreen* this) {
 
       char buffer[32] = {0};
       xSnprintf(buffer, sizeof(buffer), "%d", Process_getPid(this->super.process));
-      // Use of NULL in variadic functions must have a pointer cast.
-      // The NULL constant is not required by standard to have a pointer type.
-      execlp("strace", "strace", "-T", "-tt", "-s", "512", "-p", buffer, (char*)NULL);
 
-      // Should never reach here, unless execlp fails ...
-      const char* message = "Could not execute 'strace'. Please make sure it is available in your $PATH.";
-      (void)! write(STDERR_FILENO, message, strlen(message));
+      #if defined(HTOP_FREEBSD) || defined(HTOP_OPENBSD) || defined(HTOP_NETBSD) || defined(HTOP_DRAGONFLYBSD) || defined(HTOP_SOLARIS)
+         // Use of NULL in variadic functions must have a pointer cast.
+         // The NULL constant is not required by standard to have a pointer type.
+         execlp("truss", "truss", "-s", "512", "-p", buffer, (void*)NULL);
+
+         const char* message = "Could not execute 'truss'. Please make sure it is available in your $PATH.";
+         (void)! write(STDERR_FILENO, message, strlen(message));
+      #elif defined(HTOP_LINUX)
+         execlp("strace", "strace", "-T", "-tt", "-s", "512", "-p", buffer, NULL);
+
+         // Should never reach here, unless execlp fails ...
+         const char* message = "Could not execute 'strace'. Please make sure it is available in your $PATH.";
+         (void)! write(STDERR_FILENO, message, strlen(message));
+      #else // HTOP_DARWIN, HTOP_PCP == HTOP_UNSUPPORTED
+         const char* message = "Tracing unavailable on not supported system.";
+         (void)! write(STDERR_FILENO, message, strlen(message));
+      #endif
 
       exit(127);
    }

commit 447c72590511a191f25ea2c1d461f59d2b5103d8

Fix execlp() argument without pointer cast
Regression from c55401b82b1bb278947f9d2a3aed13a9f0de85a9

TraceScreen.c
@@ -96,10 +96,11 @@ bool TraceScreen_forkTracer(TraceScreen* this) {
          // The NULL constant is not required by standard to have a pointer type.
          execlp("truss", "truss", "-s", "512", "-p", buffer, (void*)NULL);
 
+         // Should never reach here, unless execlp fails ...
          const char* message = "Could not execute 'truss'. Please make sure it is available in your $PATH.";
          (void)! write(STDERR_FILENO, message, strlen(message));
       #elif defined(HTOP_LINUX)
-         execlp("strace", "strace", "-T", "-tt", "-s", "512", "-p", buffer, NULL);
+         execlp("strace", "strace", "-T", "-tt", "-s", "512", "-p", buffer, (void*)NULL);
 
          // Should never reach here, unless execlp fails ...
          const char* message = "Could not execute 'strace'. Please make sure it is available in your $PATH.";