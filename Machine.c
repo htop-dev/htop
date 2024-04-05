htop-dev/htop

commit b61685779cdf696ba4135a97ce66075c337c7562

Store time of the previous process scan
The difference of scans is useful for utilization calculations.

To avoid divisions by 0 on first scan set monotonicMs also on first
scan.

Machine.c
@@ -99,10 +99,14 @@ void Machine_scanTables(Machine* this) {
    // set scan timestamp
    static bool firstScanDone = false;
 
-   if (firstScanDone)
-      Platform_gettime_monotonic(&this->monotonicMs);
-   else
+   if (firstScanDone) {
+      this->prevMonotonicMs = this->monotonicMs;
+   } else {
+      this->prevMonotonicMs = 0;
       firstScanDone = true;
+   }
+   Platform_gettime_monotonic(&this->monotonicMs);
+   assert(this->monotonicMs > this->prevMonotonicMs);
 
    this->maxUserId = 0;
    Row_resetFieldWidths();

Machine.h
@@ -41,6 +41,7 @@ typedef struct Machine_ {
    struct timeval realtime;   /* time of the current sample */
    uint64_t realtimeMs;       /* current time in milliseconds */
    uint64_t monotonicMs;      /* same, but from monotonic clock */
+   uint64_t prevMonotonicMs;  /* time in milliseconds from monotonic clock of previous scan */
 
    int64_t iterationsRemaining;
 

commit 9fb01dfc2f8257961089c2c00319ce1c6d55088c

Fix regression from storing monotonic time deltas
When starting htop the timestamp of the first scan
must be far in the past to avoid showing all processes as
being started just recently.

Fixes: b61685779cdf696ba4135a97ce66075c337c7562

Machine.c
@@ -101,11 +101,12 @@ void Machine_scanTables(Machine* this) {
 
    if (firstScanDone) {
       this->prevMonotonicMs = this->monotonicMs;
+      Platform_gettime_monotonic(&this->monotonicMs);
    } else {
       this->prevMonotonicMs = 0;
+      this->monotonicMs = 1;
       firstScanDone = true;
    }
-   Platform_gettime_monotonic(&this->monotonicMs);
    assert(this->monotonicMs > this->prevMonotonicMs);
 
    this->maxUserId = 0;