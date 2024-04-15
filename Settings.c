htop-dev/htop

commit 15b4bc45b2b0ccf2d93164b2b1b05fb94cc89220

Read htoprc in modern location first before old one
Read htop configuration file in the modern location first
("~/.config/htop/htoprc") before trying the legacy location
("~/.htoprc"). This would prevent a case where configuration files exist
in both locations and the new configuration gets replaced by the old
one.

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

Settings.c
@@ -838,8 +838,8 @@ Settings* Settings_new(unsigned int initialCpuCount, Hashtable* dynamicMeters, H
 #endif
    this->changed = false;
    this->delay = DEFAULT_DELAY;
-   bool ok = false;
-   if (legacyDotfile) {
+   bool ok = Settings_read(this, this->filename, initialCpuCount);
+   if (!ok && legacyDotfile) {
       ok = Settings_read(this, legacyDotfile, initialCpuCount);
       if (ok) {
          // Transition to new location and delete old configuration file
@@ -848,9 +848,6 @@ Settings* Settings_new(unsigned int initialCpuCount, Hashtable* dynamicMeters, H
          }
       }
       free(legacyDotfile);
-   }
-   if (!ok) {
-      ok = Settings_read(this, this->filename, initialCpuCount);
    }
    if (!ok) {
       this->screenTabs = true;

commit 9e8047d7f3744c8da01f7067c6705f1d8eef07ec

Prevent a memory leak when legacy config file exists
Fix a regression caused by 15b4bc45b2b0ccf2d93164b2b1b05fb94cc89220

Resolves: #1449

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

Settings.c
@@ -815,11 +815,12 @@ Settings* Settings_new(unsigned int initialCpuCount, Hashtable* dynamicMeters, H
          configDir = String_cat(home, "/.config");
          htopDir = String_cat(home, "/.config/htop");
       }
-      legacyDotfile = String_cat(home, "/.htoprc");
       (void) mkdir(configDir, 0700);
       (void) mkdir(htopDir, 0700);
       free(htopDir);
       free(configDir);
+
+      legacyDotfile = String_cat(home, "/.htoprc");
       struct stat st;
       int err = lstat(legacyDotfile, &st);
       if (err || S_ISLNK(st.st_mode)) {
@@ -838,6 +839,7 @@ Settings* Settings_new(unsigned int initialCpuCount, Hashtable* dynamicMeters, H
 #endif
    this->changed = false;
    this->delay = DEFAULT_DELAY;
+
    bool ok = Settings_read(this, this->filename, initialCpuCount);
    if (!ok && legacyDotfile) {
       ok = Settings_read(this, legacyDotfile, initialCpuCount);
@@ -847,8 +849,9 @@ Settings* Settings_new(unsigned int initialCpuCount, Hashtable* dynamicMeters, H
             unlink(legacyDotfile);
          }
       }
-      free(legacyDotfile);
    }
+   free(legacyDotfile);
+
    if (!ok) {
       this->screenTabs = true;
       this->changed = true;