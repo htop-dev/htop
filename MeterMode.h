9f5e4fe3814d1f1cf3d50a11bc0e5ff89ac45540
Explorer09 <explorer09@gmail.com>
2024-04-18 22:07:39 +0800

    New header "MeterMode.h" for MeterModeId definitions
    This is a prerequisite to using MeterModeId type in more headers, to
    avoid header dependency hell.

---
+++ MeterMode.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_MeterMode
+#define HEADER_MeterMode
+/*
+htop - MeterMode.h
+(C) 2024 htop dev team
+Released under the GNU GPLv2+, see the COPYING file
+in the source distribution for its full text.
+*/
+
+
+typedef enum {
+   /* Meter mode 0 is reserved */
+   BAR_METERMODE = 1,
+   TEXT_METERMODE,
+   GRAPH_METERMODE,
+   LED_METERMODE,
+   LAST_METERMODE
+} MeterModeId;
+
+#endif
