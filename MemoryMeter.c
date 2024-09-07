03249f86517e5c095f077dec8b3ff1b7b8a12920
Explorer09 <explorer09@gmail.com>
2024-09-07 13:00:02 +0800

    Introduce isPercentChart member in MeterClass
    This property distinguishes meters that have a (relatively) fixed
    "total" value and meters that do not have a definite maximum value.
    The former meters would be drawn as a "100% stacked bar" or "graph", and
    the latter could have their "total" values updated automatically in bar
    meter mode.
    
    This commit is a prerequisite for the feature "Graph meter dynamic
    scaling and percent graph drawing".
    
    Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

--- MemoryMeter.c
+++ MemoryMeter.c
@@ -116,6 +116,7 @@ const MeterClass MemoryMeter_class = {
    .defaultMode = BAR_METERMODE,
    .supportedModes = METERMODE_DEFAULT_SUPPORTED,
    .maxItems = MEMORY_METER_ITEMCOUNT,
+   .isPercentChart = true,
    .total = 100.0,
    .attributes = MemoryMeter_attributes,
    .name = "Memory",
