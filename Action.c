htop-dev/htop

commit b29b33ebb9ae80a95e7d09eb537e0fccc5043d68

{Memory,Swap}Meter: add "compressed memory" metrics
For now, the semantics are mostly fit for Linux zswap subsystem. For
instance, we add the third swap usage metric that indicates the amount
of memory that is accounted to swap but in fact stored elsewhere. This
exactly matches the definition of frontswap/zswap, and is probably of
little use to all other platforms.

Action.c
@@ -674,10 +674,11 @@ static Htop_Reaction actionHelp(State* st) {
    mvaddstr(line++, 0, "Memory bar:    ");
    addattrstr(CRT_colors[BAR_BORDER], "[");
    addbartext(CRT_colors[MEMORY_USED], "", "used");
+   addbartext(CRT_colors[MEMORY_COMPRESSED], "/", "compressed");
    addbartext(CRT_colors[MEMORY_BUFFERS_TEXT], "/", "buffers");
    addbartext(CRT_colors[MEMORY_SHARED], "/", "shared");
    addbartext(CRT_colors[MEMORY_CACHE], "/", "cache");
-   addbartext(CRT_colors[BAR_SHADOW], "                     ", "used");
+   addbartext(CRT_colors[BAR_SHADOW], "          ", "used");
    addbartext(CRT_colors[BAR_SHADOW], "/", "total");
    addattrstr(CRT_colors[BAR_BORDER], "]");
 
@@ -687,10 +688,11 @@ static Htop_Reaction actionHelp(State* st) {
    addbartext(CRT_colors[SWAP], "", "used");
 #ifdef HTOP_LINUX
    addbartext(CRT_colors[SWAP_CACHE], "/", "cache");
+   addbartext(CRT_colors[SWAP_FRONTSWAP], "/", "frontswap");
 #else
    addbartext(CRT_colors[SWAP_CACHE], "      ", "");
 #endif
-   addbartext(CRT_colors[BAR_SHADOW], "                                    ", "used");
+   addbartext(CRT_colors[BAR_SHADOW], "                          ", "used");
    addbartext(CRT_colors[BAR_SHADOW], "/", "total");
    addattrstr(CRT_colors[BAR_BORDER], "]");
 

CRT.c
@@ -163,6 +163,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [BAR_SHADOW] = A_BOLD | ColorPairGrayBlack,
       [SWAP] = ColorPair(Red, Black),
       [SWAP_CACHE] = ColorPair(Yellow, Black),
+      [SWAP_FRONTSWAP] = A_BOLD | ColorPairGrayBlack,
       [GRAPH_1] = A_BOLD | ColorPair(Cyan, Black),
       [GRAPH_2] = ColorPair(Cyan, Black),
       [MEMORY_USED] = ColorPair(Green, Black),
@@ -170,6 +171,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [MEMORY_BUFFERS_TEXT] = A_BOLD | ColorPair(Blue, Black),
       [MEMORY_CACHE] = ColorPair(Yellow, Black),
       [MEMORY_SHARED] = ColorPair(Magenta, Black),
+      [MEMORY_COMPRESSED] = A_BOLD | ColorPairGrayBlack,
       [HUGEPAGE_1] = ColorPair(Green, Black),
       [HUGEPAGE_2] = ColorPair(Yellow, Black),
       [HUGEPAGE_3] = ColorPair(Red, Black),
@@ -274,6 +276,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [BAR_SHADOW] = A_DIM,
       [SWAP] = A_BOLD,
       [SWAP_CACHE] = A_NORMAL,
+      [SWAP_FRONTSWAP] = A_DIM,
       [GRAPH_1] = A_BOLD,
       [GRAPH_2] = A_NORMAL,
       [MEMORY_USED] = A_BOLD,
@@ -281,6 +284,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [MEMORY_BUFFERS_TEXT] = A_NORMAL,
       [MEMORY_CACHE] = A_NORMAL,
       [MEMORY_SHARED] = A_NORMAL,
+      [MEMORY_COMPRESSED] = A_DIM,
       [HUGEPAGE_1] = A_BOLD,
       [HUGEPAGE_2] = A_NORMAL,
       [HUGEPAGE_3] = A_REVERSE | A_BOLD,
@@ -385,6 +389,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [BAR_SHADOW] = ColorPair(Black, White),
       [SWAP] = ColorPair(Red, White),
       [SWAP_CACHE] = ColorPair(Yellow, White),
+      [SWAP_FRONTSWAP] = A_BOLD | ColorPair(Black, White),
       [GRAPH_1] = A_BOLD | ColorPair(Blue, White),
       [GRAPH_2] = ColorPair(Blue, White),
       [MEMORY_USED] = ColorPair(Green, White),
@@ -392,6 +397,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [MEMORY_BUFFERS_TEXT] = ColorPair(Cyan, White),
       [MEMORY_CACHE] = ColorPair(Yellow, White),
       [MEMORY_SHARED] = ColorPair(Magenta, White),
+      [MEMORY_COMPRESSED] = A_BOLD | ColorPair(Black, White),
       [HUGEPAGE_1] = ColorPair(Green, White),
       [HUGEPAGE_2] = ColorPair(Yellow, White),
       [HUGEPAGE_3] = ColorPair(Red, White),
@@ -496,6 +502,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [BAR_SHADOW] = ColorPairGrayBlack,
       [SWAP] = ColorPair(Red, Black),
       [SWAP_CACHE] = ColorPair(Yellow, Black),
+      [SWAP_FRONTSWAP] = ColorPairGrayBlack,
       [GRAPH_1] = A_BOLD | ColorPair(Cyan, Black),
       [GRAPH_2] = ColorPair(Cyan, Black),
       [MEMORY_USED] = ColorPair(Green, Black),
@@ -503,6 +510,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [MEMORY_BUFFERS_TEXT] = ColorPair(Cyan, Black),
       [MEMORY_CACHE] = ColorPair(Yellow, Black),
       [MEMORY_SHARED] = ColorPair(Magenta, Black),
+      [MEMORY_COMPRESSED] = ColorPairGrayBlack,
       [HUGEPAGE_1] = ColorPair(Green, Black),
       [HUGEPAGE_2] = ColorPair(Yellow, Black),
       [HUGEPAGE_3] = ColorPair(Red, Black),
@@ -607,6 +615,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [BAR_SHADOW] = ColorPair(Cyan, Blue),
       [SWAP] = ColorPair(Red, Blue),
       [SWAP_CACHE] = A_BOLD | ColorPair(Yellow, Blue),
+      [SWAP_FRONTSWAP] = A_BOLD | ColorPair(Black, Blue),
       [GRAPH_1] = A_BOLD | ColorPair(Cyan, Blue),
       [GRAPH_2] = ColorPair(Cyan, Blue),
       [MEMORY_USED] = A_BOLD | ColorPair(Green, Blue),
@@ -614,6 +623,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [MEMORY_BUFFERS_TEXT] = A_BOLD | ColorPair(Cyan, Blue),
       [MEMORY_CACHE] = A_BOLD | ColorPair(Yellow, Blue),
       [MEMORY_SHARED] = A_BOLD | ColorPair(Magenta, Blue),
+      [MEMORY_COMPRESSED] = A_BOLD | ColorPair(Black, Blue),
       [HUGEPAGE_1] = A_BOLD | ColorPair(Green, Blue),
       [HUGEPAGE_2] = A_BOLD | ColorPair(Yellow, Blue),
       [HUGEPAGE_3] = A_BOLD | ColorPair(Red, Blue),
@@ -718,6 +728,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [BAR_SHADOW] = ColorPair(Cyan, Black),
       [SWAP] = ColorPair(Red, Black),
       [SWAP_CACHE] = ColorPair(Yellow, Black),
+      [SWAP_FRONTSWAP] = ColorPair(Yellow, Black),
       [GRAPH_1] = A_BOLD | ColorPair(Green, Black),
       [GRAPH_2] = ColorPair(Green, Black),
       [MEMORY_USED] = ColorPair(Green, Black),
@@ -725,6 +736,7 @@ static int CRT_colorSchemes[LAST_COLORSCHEME][LAST_COLORELEMENT] = {
       [MEMORY_BUFFERS_TEXT] = A_BOLD | ColorPair(Blue, Black),
       [MEMORY_CACHE] = ColorPair(Yellow, Black),
       [MEMORY_SHARED] = ColorPair(Magenta, Black),
+      [MEMORY_COMPRESSED] = ColorPair(Yellow, Black),
       [HUGEPAGE_1] = ColorPair(Green, Black),
       [HUGEPAGE_2] = ColorPair(Yellow, Black),
       [HUGEPAGE_3] = ColorPair(Red, Black),

CRT.h
@@ -68,6 +68,7 @@ typedef enum ColorElements_ {
    TASKS_RUNNING,
    SWAP,
    SWAP_CACHE,
+   SWAP_FRONTSWAP,
    PROCESS,
    PROCESS_SHADOW,
    PROCESS_TAG,
@@ -95,6 +96,7 @@ typedef enum ColorElements_ {
    MEMORY_BUFFERS_TEXT,
    MEMORY_CACHE,
    MEMORY_SHARED,
+   MEMORY_COMPRESSED,
    HUGEPAGE_1,
    HUGEPAGE_2,
    HUGEPAGE_3,

MemoryMeter.c
@@ -20,6 +20,7 @@ static const int MemoryMeter_attributes[] = {
    MEMORY_USED,
    MEMORY_BUFFERS,
    MEMORY_SHARED,
+   MEMORY_COMPRESSED,
    MEMORY_CACHE
 };
 
@@ -28,8 +29,9 @@ static void MemoryMeter_updateValues(Meter* this) {
    size_t size = sizeof(this->txtBuffer);
    int written;
 
-   /* shared and available memory are not supported on all platforms */
+   /* shared, compressed and available memory are not supported on all platforms */
    this->values[MEMORY_METER_SHARED] = NAN;
+   this->values[MEMORY_METER_COMPRESSED] = NAN;
    this->values[MEMORY_METER_AVAILABLE] = NAN;
    Platform_setMemoryValues(this);
 
@@ -38,7 +40,13 @@ static void MemoryMeter_updateValues(Meter* this) {
       "MEMORY_METER_AVAILABLE is not the last item in MemoryMeterValues");
    this->curItems = MEMORY_METER_AVAILABLE;
 
-   written = Meter_humanUnit(buffer, this->values[MEMORY_METER_USED], size);
+   /* we actually want to show "used + compressed" */
+   double used = this->values[MEMORY_METER_USED];
+   if (!isnan(this->values[MEMORY_METER_COMPRESSED])) {
+      used += this->values[MEMORY_METER_COMPRESSED];
+   }
+
+   written = Meter_humanUnit(buffer, used, size);
    METER_BUFFER_CHECK(buffer, size, written);
 
    METER_BUFFER_APPEND_CHR(buffer, size, '/');
@@ -67,6 +75,13 @@ static void MemoryMeter_display(const Object* cast, RichString* out) {
       Meter_humanUnit(buffer, this->values[MEMORY_METER_SHARED], sizeof(buffer));
       RichString_appendAscii(out, CRT_colors[METER_TEXT], " shared:");
       RichString_appendAscii(out, CRT_colors[MEMORY_SHARED], buffer);
+   }
+
+   /* compressed memory is not supported on all platforms */
+   if (!isnan(this->values[MEMORY_METER_COMPRESSED])) {
+      Meter_humanUnit(buffer, this->values[MEMORY_METER_COMPRESSED], sizeof(buffer));
+      RichString_appendAscii(out, CRT_colors[METER_TEXT], " compressed:");
+      RichString_appendAscii(out, CRT_colors[MEMORY_COMPRESSED], buffer);
    }
 
    Meter_humanUnit(buffer, this->values[MEMORY_METER_CACHE], sizeof(buffer));

MemoryMeter.h
@@ -13,9 +13,10 @@ typedef enum {
    MEMORY_METER_USED = 0,
    MEMORY_METER_BUFFERS = 1,
    MEMORY_METER_SHARED = 2,
-   MEMORY_METER_CACHE = 3,
-   MEMORY_METER_AVAILABLE = 4,
-   MEMORY_METER_ITEMCOUNT = 5, // number of entries in this enum
+   MEMORY_METER_COMPRESSED = 3,
+   MEMORY_METER_CACHE = 4,
+   MEMORY_METER_AVAILABLE = 5,
+   MEMORY_METER_ITEMCOUNT = 6, // number of entries in this enum
 } MemoryMeterValues;
 
 extern const MeterClass MemoryMeter_class;

SwapMeter.c
@@ -20,7 +20,8 @@ in the source distribution for its full text.
 
 static const int SwapMeter_attributes[] = {
    SWAP,
-   SWAP_CACHE
+   SWAP_CACHE,
+   SWAP_FRONTSWAP,
 };
 
 static void SwapMeter_updateValues(Meter* this) {
@@ -29,6 +30,7 @@ static void SwapMeter_updateValues(Meter* this) {
    int written;
 
    this->values[SWAP_METER_CACHE] = NAN;   /* 'cached' not present on all platforms */
+   this->values[SWAP_METER_FRONTSWAP] = NAN;   /* 'frontswap' not present on all platforms */
    Platform_setSwapValues(this);
 
    written = Meter_humanUnit(buffer, this->values[SWAP_METER_USED], size);
@@ -53,6 +55,12 @@ static void SwapMeter_display(const Object* cast, RichString* out) {
       Meter_humanUnit(buffer, this->values[SWAP_METER_CACHE], sizeof(buffer));
       RichString_appendAscii(out, CRT_colors[METER_TEXT], " cache:");
       RichString_appendAscii(out, CRT_colors[SWAP_CACHE], buffer);
+   }
+
+   if (!isnan(this->values[SWAP_METER_FRONTSWAP])) {
+      Meter_humanUnit(buffer, this->values[SWAP_METER_FRONTSWAP], sizeof(buffer));
+      RichString_appendAscii(out, CRT_colors[METER_TEXT], " frontswap:");
+      RichString_appendAscii(out, CRT_colors[SWAP_FRONTSWAP], buffer);
    }
 }
 

SwapMeter.h
@@ -12,7 +12,8 @@ in the source distribution for its full text.
 typedef enum {
    SWAP_METER_USED = 0,
    SWAP_METER_CACHE = 1,
-   SWAP_METER_ITEMCOUNT = 2, // number of entries in this enum
+   SWAP_METER_FRONTSWAP = 2,
+   SWAP_METER_ITEMCOUNT = 3, // number of entries in this enum
 } SwapMeterValues;
 
 extern const MeterClass SwapMeter_class;

darwin/Platform.c
@@ -294,6 +294,7 @@ void Platform_setMemoryValues(Meter* mtr) {
    mtr->values[MEMORY_METER_USED] = (double)(vm->active_count + vm->wire_count) * page_K;
    mtr->values[MEMORY_METER_BUFFERS] = (double)vm->purgeable_count * page_K;
    // mtr->values[MEMORY_METER_SHARED] = "shared memory, like tmpfs and shm"
+   // mtr->values[MEMORY_METER_COMPRESSED] = "compressed memory, like zswap on linux"
    mtr->values[MEMORY_METER_CACHE] = (double)vm->inactive_count * page_K;
    // mtr->values[MEMORY_METER_AVAILABLE] = "available memory"
 }
@@ -306,6 +307,8 @@ void Platform_setSwapValues(Meter* mtr) {
 
    mtr->total = swapused.xsu_total / 1024;
    mtr->values[SWAP_METER_USED] = swapused.xsu_used / 1024;
+   // mtr->values[SWAP_METER_CACHE] = "pages that are both in swap and RAM, like SwapCached on linux"
+   // mtr->values[SWAP_METER_FRONTSWAP] = "pages that are accounted to swap but stored elsewhere, like frontswap on linux"
 }
 
 void Platform_setZfsArcValues(Meter* this) {

commit c46d0f292b34987a941a1c29bcd8b03fd6b53ded

Fix "Swap bar" alignment in help screen
Regression from b29b33ebb9ae80a95e7d09eb537e0fccc5043d68
The misaligned text is only seen on non-Linux systems.

Action.c
@@ -758,7 +758,7 @@ static Htop_Reaction actionHelp(State* st) {
    addbartext(CRT_colors[SWAP_CACHE], "/", "cache");
    addbartext(CRT_colors[SWAP_FRONTSWAP], "/", "frontswap");
 #else
-   addbartext(CRT_colors[SWAP_CACHE], "      ", "");
+   addbartext(CRT_colors[BAR_SHADOW], "                ", "");
 #endif
    addbartext(CRT_colors[BAR_SHADOW], "                          ", "used");
    addbartext(CRT_colors[BAR_SHADOW], "/", "total");