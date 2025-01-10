2cfea7a8943cf433cf7ce5ea374aac6fb1761ba8
Explorer09 <explorer09@gmail.com>
2025-01-11 05:31:50 +0800

    Reflect the "show cached memory" setting in help screen
    Hide the "buffers/cache" text on the memory bar description when the
    "show cached memory" setting is off.
    
    Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

--- Action.c
+++ Action.c
@@ -744,9 +744,13 @@ static Htop_Reaction actionHelp(State* st) {
    addbartext(CRT_colors[MEMORY_USED], "", "used");
    addbartext(CRT_colors[MEMORY_SHARED], "/", "shared");
    addbartext(CRT_colors[MEMORY_COMPRESSED], "/", "compressed");
-   addbartext(CRT_colors[MEMORY_BUFFERS_TEXT], "/", "buffers");
-   addbartext(CRT_colors[MEMORY_CACHE], "/", "cache");
-   addbartext(CRT_colors[BAR_SHADOW], "          ", "used");
+   if (st->host->settings->showCachedMemory) {
+      addbartext(CRT_colors[MEMORY_BUFFERS_TEXT], "/", "buffers");
+      addbartext(CRT_colors[MEMORY_CACHE], "/", "cache");
+      addbartext(CRT_colors[BAR_SHADOW], "          ", "used");
+   } else {
+      addbartext(CRT_colors[BAR_SHADOW], "                        ", "used");
+   }
    addbartext(CRT_colors[BAR_SHADOW], "/", "total");
    addattrstr(CRT_colors[BAR_BORDER], "]");
 
