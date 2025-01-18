9ff7e05e84b5e07789dbe6fe6a89c0e05c83fb67
Explorer09 <explorer09@gmail.com>
2025-01-19 05:33:59 +0800

    Panel.scrollH value clamp logic improvement
    The Panel.scrollH value should have a minimum of 0 in the KEY_LEFT
    event.
    
    Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

--- Panel.c
+++ Panel.c
@@ -386,7 +386,7 @@ bool Panel_onKey(Panel* this, int key) {
       case KEY_LEFT:
       case KEY_CTRL('B'):
          if (this->scrollH > 0) {
-            this->scrollH -= MAXIMUM(CRT_scrollHAmount, 0);
+            this->scrollH = MAXIMUM(this->scrollH - CRT_scrollHAmount, 0);
             this->needsRedraw = true;
          }
          break;
