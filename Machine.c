2087ca52060d16b27de89920bc65f56fc6818bb4
Explorer09 <explorer09@gmail.com>
2025-03-10 04:50:28 +0800

    Prevent assertion error in Machine_scanTables()
    ... when the function is called in quick succession (monotonic time
    < 1 millisecond).

--- Machine.c
+++ Machine.c
@@ -107,7 +107,9 @@ void Machine_scanTables(Machine* this) {
       this->monotonicMs = 1;
       firstScanDone = true;
    }
-   assert(this->monotonicMs > this->prevMonotonicMs);
+   if (this->monotonicMs <= this->prevMonotonicMs) {
+      return;
+   }
 
    this->maxUserId = 0;
    Row_resetFieldWidths();
