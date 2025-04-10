htop-dev/htop

commit 3cfc28968feee6ebbbf2058f2046ba0bab87c89f

Use 'int' type for backtrace() return value
The libc backtrace() API uses 'int' type for "number of pointers"
return value, and backtrace_symbols_fd() accepts this value as
argument in 'int' type, too. Using size_t would unnecessarily generate
a "-Wshorten-64-to-32" warning in Clang 19.

CRT.c
@@ -1260,8 +1260,8 @@ static void print_backtrace(void) {
 #elif defined(HAVE_EXECINFO_H)
    void* backtraceArray[256];
 
-   size_t size = backtrace(backtraceArray, ARRAYSIZE(backtraceArray));
-   backtrace_symbols_fd(backtraceArray, size, STDERR_FILENO);
+   int nptrs = backtrace(backtraceArray, ARRAYSIZE(backtraceArray));
+   backtrace_symbols_fd(backtraceArray, nptrs, STDERR_FILENO);
 #else
 #error No implementation for print_backtrace()!
 #endif

commit ac84f46452854eae0e40c76839ef021b938a955f

build: Automatically detect backtrace(3) return type
The return type of the backtrace() function is actually different
among operating systems. Rather than fix it to 'int' or 'size_t',
detect it automatically in the configure script.

The automatic type of backtrace() would prevent the warning
"-Wshorten-64-to-32" in Clang for implicitly converting 'size_t' to
'int', either during the return of backtrace() or the call of
backtrace_symbols_fd() (when passing the length argument as input).

Regression from 3cfc28968feee6ebbbf2058f2046ba0bab87c89f

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

CRT.c
@@ -35,7 +35,7 @@ in the source distribution for its full text.
 # if defined(HAVE_DLADDR)
 #  include <dlfcn.h>
 # endif
-#elif defined(HAVE_EXECINFO_H)
+#elif defined(HAVE_EXECINFO_H) && defined(BACKTRACE_RETURN_TYPE)
 # define PRINT_BACKTRACE
 # include <execinfo.h>
 #endif
@@ -1257,10 +1257,10 @@ static void print_backtrace(void) {
       snprintf(err_buf, sizeof(err_buf), "%2u: %#14lx  %s  (%s+%#lx)  [%p]%s\n", item++, pc, fname, symbolName, offset, ptr, frame);
       full_write_str(STDERR_FILENO, err_buf);
    }
-#elif defined(HAVE_EXECINFO_H)
+#elif defined(HAVE_EXECINFO_H) && defined(BACKTRACE_RETURN_TYPE)
    void* backtraceArray[256];
 
-   int nptrs = backtrace(backtraceArray, ARRAYSIZE(backtraceArray));
+   BACKTRACE_RETURN_TYPE nptrs = backtrace(backtraceArray, ARRAYSIZE(backtraceArray));
    if (nptrs > 0) {
       backtrace_symbols_fd(backtraceArray, nptrs, STDERR_FILENO);
    } else {

configure.ac
@@ -833,6 +833,37 @@ esac
 if test "x$enable_unwind" = xno; then
    # Fall back to backtrace(3) and add -lexecinfo if needed
    AC_SEARCH_LIBS([backtrace], [execinfo])
+
+   htop_save_CFLAGS=$CFLAGS
+   CFLAGS="$AM_CFLAGS $CFLAGS -Werror"
+
+   # Check the return type of backtrace(3)
+   # FreeBSD, NetBSD and OpenBSD use 'size_t'.
+   # Linux and macOS use 'int'.
+   # See also: Gnulib documentation of 'backtrace'
+   # (https://www.gnu.org/software/gnulib/manual/html_node/backtrace.html)
+   for backtrace_return_type in \
+      "typeof(backtrace((void**)0, 0))" \
+      "__typeof__(backtrace((void**)0, 0))" \
+      "__typeof(backtrace((void**)0, 0))" \
+      size_t \
+      int \
+   ; do
+      AC_LINK_IFELSE([AC_LANG_PROGRAM([[
+#include <execinfo.h>
+#include <stddef.h> /* For size_t */
+         ]],[[
+/* Many compilers now make incompatible function pointer types an error. */
+char** (*func)(void* const*, $backtrace_return_type) = backtrace_symbols;
+static void* addr;
+$backtrace_return_type nptrs = backtrace(&addr, 1);
+(void)nptrs;
+         ]])],
+         [AC_DEFINE_UNQUOTED([BACKTRACE_RETURN_TYPE], [$backtrace_return_type], [Defined to the return type of the 'backtrace' function.])
+         break])
+   done
+
+   CFLAGS=$htop_save_CFLAGS
 fi
 
 