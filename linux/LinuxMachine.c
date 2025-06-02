67eafb36202478858c37377c3b74684d91cfb4d5
Benny Baumann <BenBE@geshi.org>
2025-06-02 14:08:04 +0200

    Handle special cases for CPU frequency data in /proc/cpuinfo
    This adds special cases for:
    
    - loongarch: https://github.com/torvalds/linux/blob/v6.15/arch/loongarch/kernel/proc.c#L42
    - s390: https://github.com/torvalds/linux/blob/v6.15/arch/s390/kernel/processor.c#L335
    
    These differ from all the other platforms in the Linux kernel and
    weren't matched properly previously. There's an additional value
    of "CPU MHz static" available for s390, which is always given
    AFTER the dynamic value and thus would override the dynamic value;
    we thus ignore the static CPU frequency given for s390 in favour of
    the dynamic entry.
    
    Fixes: #1711

--- linux/LinuxMachine.c
+++ linux/LinuxMachine.c
@@ -594,6 +594,8 @@ static void scanCPUFrequencyFromCPUinfo(LinuxMachine* this) {
          continue;
       } else if (
          (sscanf(buffer, "cpu MHz : %lf", &frequency) == 1) ||
+         (sscanf(buffer, "CPU MHz : %lf", &frequency) == 1) || // LooooongArch: https://github.com/torvalds/linux/blob/v6.15/arch/loongarch/kernel/proc.c#L42
+         (sscanf(buffer, "cpu MHz dynamic : %lf", &frequency) == 1) || // s390: https://github.com/torvalds/linux/blob/v6.15/arch/s390/kernel/processor.c#L335
          (sscanf(buffer, "clock : %lfMHz", &frequency) == 1)
       ) {
          if (cpuid < 0 || (unsigned int)cpuid > (super->existingCPUs - 1)) {
