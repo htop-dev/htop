e647a054011a80d71e43f00154da82794daa89b1
Explorer09 <explorer09@gmail.com>
2026-01-28 17:58:51 +0800

    Don't let '--no-color' affect htoprc
    Change the '--no-color' option behavior so that it can restore the color
    scheme from the htoprc file after the CRT_init() call. In other words,
    the '--no-color' override to the color scheme no longer saves to the
    htoprc (until the users changes it in the Setup UI).
    
    It is now possible for 'settings->colorScheme' to differ from the
    currently active 'CRT_colorScheme'. So ColorsPanel now fetches the
    current color scheme from 'CRT_colorScheme'.
    
    Also included in this patch is a minor simplification of CRT_init()
    code.
    
    Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

--- ColorsPanel.c
+++ ColorsPanel.c
@@ -104,6 +104,6 @@ ColorsPanel* ColorsPanel_new(Settings* settings) {
    for (int i = 0; ColorSchemeNames[i] != NULL; i++) {
       Panel_add(super, (Object*) CheckItem_newByVal(ColorSchemeNames[i], false));
    }
-   CheckItem_set((CheckItem*)Panel_get(super, settings->colorScheme), true);
+   CheckItem_set((CheckItem*)Panel_get(super, (int)CRT_colorScheme), true);
    return this;
 }
