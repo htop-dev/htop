99d2851ebece95b209877b13c83c1b8d7f4ca2a2
Edgar Bonet <linux@edgar-bonet.org>
2026-02-05 15:09:38 +0000

    Linux: fix detection of NUL argument separator
    Some programs, such as php-fpm and sshd-session, rewrite their command
    line and pad it with NUL characters. When this happens,
    LinuxProcessTable_readCmdlineFile() believes that NUL is used as an
    argument separator, and fails to invoke the argument parsing heuristic
    designed to handle this situation.
    
    Only assume NUL is an argument separator if a non-NUL character is found
    after the first NUL.
    
    Fixes: 1f664ee2 ("Properly track if we actually hit any token while scanning cmdline")
    Closes: #1817

--- linux/LinuxProcessTable.c
+++ linux/LinuxProcessTable.c
@@ -1288,11 +1288,6 @@ static bool LinuxProcessTable_readCmdlineFile(Process* process, openat_arg_t pro
    bool argSepSpace = false;
 
    for (size_t i = 0; i < (size_t)amtRead; i++) {
-      // If this is true, there's a NUL byte in the middle of command
-      if (tokenEnd != (size_t)-1) {
-         argSepNUL = true;
-      }
-
       const char argChar = command[i];
 
       /* newline used as delimiter - when forming the mergedCommand, newline is
@@ -1312,6 +1307,11 @@ static bool LinuxProcessTable_readCmdlineFile(Process* process, openat_arg_t pro
          }
 
          continue;
+      }
+
+      // If this is true, there's a NUL byte in the middle of command
+      if (tokenEnd != (size_t)-1) {
+         argSepNUL = true;
       }
 
       /* Record some information for the argument parsing heuristic below. */
