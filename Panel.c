763e498d8b7148de670b0e95bc8940c9d46bae44
Daniel Lange <DLange@git.local>
2026-09-17 09:19:46 +0200

    Harden left horizontal scrolling
    Closes #2103
    (we leave the infinite right horizontal scrolling for now)

--- Panel.c
+++ Panel.c
@@ -415,6 +415,7 @@ bool Panel_onKey(Panel* this, int key) {
       case KEY_CTRL('B'):
          if (this->scrollH > 0) {
             this->scrollH -= MAXIMUM(CRT_scrollHAmount, 0);
+            this->scrollH = MAXIMUM(this->scrollH, 0);
             this->needsRedraw = true;
          }
          break;
